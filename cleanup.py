d0b5f8d8230c68f8e8c7dc6030a2d3b83475baa0
Doron Shapiro <shapiro.doron@gmail.com>
2014-09-19 18:48:28 -0400

    Initial commit

---
+++ cleanup.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import zipfile
+
+SUBMISSIONS_FOLDER = "submissions"
+
+def make_student_directories():
+	num_students = 0
+	for file in os.listdir(os.curdir):
+		if file[0] != '.' and os.path.isfile(file):
+			student_name = file.split("_")[0]
+			if not os.path.isdir(student_name):
+				os.mkdir(student_name)
+				num_students += 1
+			shutil.move(file, student_name)
+	print num_students, " directories created"
+
+def unzip_submissions():
+	num_unzipped = 0
+	for file in os.listdir(os.curdir):
+		if file[0] != '.' and os.path.isdir(file):
+			for student_file in os.listdir(file):
+				if os.path.splitext(student_file)[1] == ".zip":
+					os.chdir(file)
+					zipped_file = zipfile.ZipFile(student_file)
+					zipped_file.extractall()
+					num_unzipped += 1
+					os.chdir(os.pardir)
+	print num_unzipped, " files unzipped"
+
+if __name__ == '__main__':
+	os.chdir(SUBMISSIONS_FOLDER)
+	make_student_directories()
+	unzip_submissions()
